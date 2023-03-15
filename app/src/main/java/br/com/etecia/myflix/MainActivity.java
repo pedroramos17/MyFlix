4cb2931f13585a0bc1f171b30c3853cb363552c8
pedroramos17 <pedro_ramos2003@hotmail.com>
2023-03-14 22:14:50 -0300

    Criando o construtor e recycleView

--- app/src/main/java/br/com/etecia/myflix/MainActivity.java
+++ app/src/main/java/br/com/etecia/myflix/MainActivity.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    List<Filmes> listaFilmes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //criando a base de dados para carregamento da lista de filmes
+        listaFilmes = new ArrayList<>();
+
+        listaFilmes.add(
+                new Filmes("Creed", "Adonis Johnson never knew his famous father, world heavyweight champion Apollo Creed, who died before he was born.",
+                        "Drama",
+                        R.drawable.creed)
+        );
+
+        //criando a classe adaptadora e passando os parâmetros
+        MyAdapter adapter = new MyAdapter(getApplicationContext(), listaFilmes);
     }
 }
--- app/src/main/java/br/com/etecia/myflix/MyAdapter.java
+++ app/src/main/java/br/com/etecia/myflix/MyAdapter.java
@@ -1,12 +1,26 @@
 package br.com.etecia.myflix;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private Context mContexto;
+    private List<Filmes> listaFilmes;
+
+    public MyAdapter(Context mContexto, List<Filmes> listFilmes) {
+        this.mContexto = mContexto;
+        this.listaFilmes = listaFilmes;
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -22,12 +36,19 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     public int getItemCount() {
         return 0;
     }
+    TextView idTituloFilmes;
+    ImageView idImagemFilmes;
+    CardView idCardFilmes;
+
 
     //criando a classe ViewHolder
     public class ViewHolder extends RecyclerView.ViewHolder{
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            idTituloFilmes = itemView.findViewById(R.id.idTituloFilmes);
+            idImagemFilmes = itemView.findViewById(R.id.idImagemFilmes);
+            idCardFilmes = itemView.findViewById(R.id.idCardFilmes);
         }
     }
 }
